211a72bff8706f44cd38cd8747f66846588c1328
andder-1 <anddergpq@gmail.com>
2024-08-10 14:41:40 -0500

    Bubiendo proyecto botton magic

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+    <div className="flex justify-center items-center h-screen">
+      <a className="link"href="">Hover</a>
+      
+    </div>
+    </>
+  );
+};
+export default App;
